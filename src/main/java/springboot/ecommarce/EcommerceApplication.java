35bee9952d3b68fe3cb671bd7edd71d0c6c251c0
Aman Ullah <aman.jessore@gmail.com>
2021-07-08 17:24:29 +0600

    initial commit

---
+++ src/main/java/springboot/ecommarce/EcommerceApplication.java
@@ -0,0 +1,14 @@
+package springboot.ecommarce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/springboot/ecommarce/model/User.java
@@ -0,0 +1,28 @@
+package springboot.ecommarce.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "password")
+	private String password;
+
+	@Column(name = "type")
+	private String type;
+}
